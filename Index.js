a7b182a210bee4e0b0ff31e6c44fb62e13b18447
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-01-26 11:36:52 +0600

    create founder delete route

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -43,6 +43,13 @@ async function run() {
 
     app.get("/founders", async (req, res) => {
       const result = await foundersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/founders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foundersCollection.deleteOne(query);
       res.send(result);
     });
 
