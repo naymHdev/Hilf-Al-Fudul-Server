c674ee97e640dec00daa83b933925de2a05696f0
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-02-09 19:57:17 +0600

    create a delete route on vonlunter section

--- Index.js
+++ Index.js
@@ -47,6 +47,13 @@ async function run() {
       res.send(result);
     });
 
+     app.delete("/joinVolunteer/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // founders routes
     app.post("/founders", async (req, res) => {
       const person = req.body;
