ae22fa78c5f91b5e26d553446697ed7f485a9486
MacBook Pro <naym100m@gmail.com>
2024-08-23 01:18:34 +0600

    created a new route

--- Index.js
+++ Index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 8000;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
     "https://hilf-al-fudul.netlify.app",
     "https://hilful-fujul-client.vercel.app",
   ],
